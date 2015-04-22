4ce08d28d25cfc44bba347ff04bff91c282eb064
Pepper <pepper@scannerjammer.com>
2015-04-21 21:31:05 -0400

    added test everything

---
+++ WordSynth/test_all.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python2.7
+import unittest
+testmodules = [
+    'Test.WordSyllable',
+    ]
+
+suite = unittest.TestSuite()
+
+for t in testmodules:
+    try:
+        # If the module defines a suite() function, call it to get the suite.
+        mod = __import__(t, globals(), locals(), ['suite'])
+        suitefn = getattr(mod, 'suite')
+        suite.addTest(suitefn())
+    except (ImportError, AttributeError):
+        # else, just load all the test cases from the module.
+        suite.addTest(unittest.defaultTestLoader.loadTestsFromName(t))
+
+unittest.TextTestRunner().run(suite)
