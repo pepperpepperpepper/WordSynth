4818c97d845c2b585720b4061147d88ff5be95f9
yo mama <pepper@scannerjammer.com>
2015-06-14 23:37:15 -0700

    ok

---
+++ WordSynth/Note/WordMapper/__init__.py
@@ -0,0 +1,9 @@
+from Note.SyllableMapper import NoteSyllableMapper
+from Note import Note
+from Note.WordMapper.Distribution import NoteWordMapperDistribution
+
+class Note.WordMapper(object):
+  def __init__():
+    pass
+  def map_words(words, notes, distrubtion="tail"):
+    pass
